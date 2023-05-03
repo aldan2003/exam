c9dd838a5972ca5dd538b151e77ad3879e335646
exam <exam@Bcalab4-m05>
2023-05-03 09:34:25 +0530

    first commit

---
+++ CaseToolsExam/src/aldan/Car_105.java
@@ -0,0 +1,15 @@
+package aldan;
+
+public class Car_105 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ }
+	int speed;
+	
+	Car_105(int speed)
+	{
+		this.speed=speed;
+	}
+
+}
